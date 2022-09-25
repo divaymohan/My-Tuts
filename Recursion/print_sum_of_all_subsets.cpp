4fb55b2ecbb9b7d515d385c87d76a74f1b156aef
dm255078 <divaymohan05@gmail.com>
2022-09-25 10:36:35 +0530

    minor change

--- Recursion/print_sum_of_all_subsets.cpp
+++ Recursion/print_sum_of_all_subsets.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void count_all_subsequences_with_sum(int n,vector<int> &in, int c_sum, vector<int> &res, int ind){
+void sum_of_all_subsets(int n,vector<int> &in, int c_sum, vector<int> &res, int ind){
    if(ind>=n){
       //add sum to list
       res.push_back(c_sum);
@@ -10,11 +10,11 @@ void count_all_subsequences_with_sum(int n,vector<int> &in, int c_sum, vector<in
    }
    //include ind
    c_sum+=in[ind];
-   count_all_subsequences_with_sum(n,in,c_sum,res,ind+1);
+   sum_of_all_subsets(n,in,c_sum,res,ind+1);
 
    c_sum-=in[ind];
    //don't include ind
-   count_all_subsequences_with_sum(n,in,c_sum,res,ind+1);
+   sum_of_all_subsets(n,in,c_sum,res,ind+1);
 
 }
 
@@ -31,7 +31,7 @@ int main(){
    }
    int c_sum=0;
    vector<int> res;
-   count_all_subsequences_with_sum(n, in, c_sum, res, 0);
+   sum_of_all_subsets(n, in, c_sum, res, 0);
    sort(res.begin(),res.end());
    for(auto item: res) cout<<item<<" ";
 
